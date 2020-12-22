d8c8888e624f40c0a4a630c526a3fdbac1dc0287
canacar9407 <canacar94@gmail.com>
2020-12-22 22:21:38 +0000

    Ödev Tamalandı

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const data = {
-  USD: {EUR: 0.82, GBP: 0.74},
-  EUR: {USD: 1.23, GBP: 0.91},
-  GBP: {USD: 1.35, EUR: 1.10},
+    USD: {EUR: 0.82, GBP: 0.74, TRY: 7.66},
+    EUR: {USD: 1.23, GBP: 0.91, TRY: 9.37},
+    GBP: {USD: 1.35, EUR: 1.10, TRY: 10.30},
+    TRY: {USD: 0.13, EUR: 0.11, GBP: 0.097}
 };
 
 const currencyKeys = Object.keys(data);
 
 function createCurrencyElements(elements, root, inputName){
-  for(let i =0; i< elements.length; i++){
-    const currencyKeyDiv   = document.createElement("div");
-    const currencyKeyInput = document.createElement("input");
-    currencyKeyInput.setAttribute("type", "radio");
-    currencyKeyInput.setAttribute("name", inputName);
-    currencyKeyInput.setAttribute("id", inputName + elements[i]);
-    currencyKeyInput.setAttribute("value", elements[i]);
+    for(let i =0; i< elements.length; i++){
+        const currencyKeyDiv   = document.createElement("div");
+        const currencyKeyInput = document.createElement("input");
+        currencyKeyInput.setAttribute("type", "radio");
+        currencyKeyInput.setAttribute("name", inputName);
+        currencyKeyInput.setAttribute("id", inputName + elements[i]);
+        currencyKeyInput.setAttribute("value", elements[i]);
 
-    const currencyKeyLabel = document.createElement("label");
-    currencyKeyLabel.setAttribute("for", inputName + elements[i]);
-    currencyKeyLabel.textContent = elements[i];
+        const currencyKeyLabel = document.createElement("label");
+        currencyKeyLabel.setAttribute("for", inputName + elements[i]);
+        currencyKeyLabel.textContent = elements[i];
 
-    currencyKeyDiv.appendChild(currencyKeyInput);
-    currencyKeyDiv.appendChild(currencyKeyLabel);
-    root.appendChild(currencyKeyDiv);
-  }
+        currencyKeyDiv.appendChild(currencyKeyInput);
+        currencyKeyDiv.appendChild(currencyKeyLabel);
+        root.appendChild(currencyKeyDiv);
+    }
 }
 
 //from
@@ -38,17 +39,34 @@ createCurrencyElements(currencyKeys, parentToEl, toInputName);
 
 const calculateButton = document.querySelector("#calculate-button");
 calculateButton.addEventListener("click", function(){
-   // kimden ceviriyourz
-   const fromTarget = document.querySelector("input[name='currency_from']:checked").value;
-   // kime ceviriyoruz
-   const toTarget   = document.querySelector("input[name='currency_to']:checked").value;
-   // amountu alalim
-   const amount     = document.querySelector("input[name='amount']").value;
+    let getSelectedValue_from = document.querySelector("input[name='currency_from']:checked");
+    let getSelectedValue_to = document.querySelector("input[name='currency_to']:checked");
+    const currencyResult = document.querySelector("#currency-result");
+    if( getSelectedValue_from == null || getSelectedValue_to == null){
+        currencyResult.innerHTML = "Seçim Yapmalısınız";
+        return;
+    }
+    // kimden ceviriyourz
+    const fromTarget = document.querySelector("input[name='currency_from']:checked").value;
+    // kime ceviriyoruz
+    const toTarget   = document.querySelector("input[name='currency_to']:checked").value;
 
-   const currentCurrencyObject = data[fromTarget];
-   const resultForOne = currentCurrencyObject[toTarget];
-   const result = amount * resultForOne;
+    // amountu alalim
+    const amount     = document.querySelector("input[name='amount']").value;
+    if (isNaN(amount)){
+        currencyResult.innerHTML = "Amount Sayı olarak Girilmeli";
+        return;
+    }
 
-   const currencyResult = document.querySelector("#currency-result");
-   currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
+    const currentCurrencyObject = data[fromTarget];
+    const resultForOne = currentCurrencyObject[toTarget];
+
+    if (data[fromTarget] === data[toTarget]) {
+        currencyResult.innerHTML = " Farklı Seçimler Yapmalısınız";
+    } else {
+        const result = amount * resultForOne;
+
+        currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
+    }
+
 });
